87dafbbb5079b87a9f18e6bd4c351b513350372c
Imran Hossain <imran096@gmail.com>
2020-08-09 01:05:26 +0600

    Updates

---
+++ precache-manifest.f7947088ce39e279fb6c8afd361d9292.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245014de797b74b6e6eb301ce76ea54f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fe49c8852896c0010397",
+    "url": "/portfolio/static/css/2.c2b0cc68.chunk.css"
+  },
+  {
+    "revision": "b6a79f5e9b5ba3faf717",
+    "url": "/portfolio/static/css/main.daaca2d5.chunk.css"
+  },
+  {
+    "revision": "fe49c8852896c0010397",
+    "url": "/portfolio/static/js/2.3e670069.chunk.js"
+  },
+  {
+    "revision": "57d433a3cf738d4f2587da1d9252fc66",
+    "url": "/portfolio/static/js/2.3e670069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a79f5e9b5ba3faf717",
+    "url": "/portfolio/static/js/main.6cdb1b4a.chunk.js"
+  },
+  {
+    "revision": "d0b438f08e766a79830a",
+    "url": "/portfolio/static/js/runtime-main.e35c117d.js"
+  },
+  {
+    "revision": "3b55d37808944aaa83cfe2428fd5e673",
+    "url": "/portfolio/static/media/1-470x490.3b55d378.jpg"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/portfolio/static/media/Linearicons.2f3e9f80.ttf"
+  },
+  {
+    "revision": "a07b3c43e781ebf33ac26fcf2141dfef",
+    "url": "/portfolio/static/media/bg1.a07b3c43.jpg"
+  },
+  {
+    "revision": "50ebb7bd0fdb7d2813bcf1ec310b3bc0",
+    "url": "/portfolio/static/media/bg2-2.50ebb7bd.jpg"
+  },
+  {
+    "revision": "06aeb2619a7369ce92878c8925bd1fe5",
+    "url": "/portfolio/static/media/bg2-3.06aeb261.jpg"
+  },
+  {
+    "revision": "8ccd4a9b56af8a5306536d0fdad7efc4",
+    "url": "/portfolio/static/media/bg2-4.8ccd4a9b.jpg"
+  },
+  {
+    "revision": "1916a9b41887622514aa1baf89d51d77",
+    "url": "/portfolio/static/media/bg2-5.1916a9b4.jpg"
+  },
+  {
+    "revision": "c1c1bdaad993ed931c3e4e68d0a5ada7",
+    "url": "/portfolio/static/media/bg2-6.c1c1bdaa.jpg"
+  },
+  {
+    "revision": "a230fb04dc49818151e8c805fa2e2289",
+    "url": "/portfolio/static/media/bg2.a230fb04.jpg"
+  },
+  {
+    "revision": "9bc337f51b65ff3fb6961c43db8dd20f",
+    "url": "/portfolio/static/media/bg3.9bc337f5.jpg"
+  },
+  {
+    "revision": "8e460e439e16af2f4e11675bed6bb104",
+    "url": "/portfolio/static/media/bg4.8e460e43.jpg"
+  },
+  {
+    "revision": "4d888edaaa918b76208525a44fbd1202",
+    "url": "/portfolio/static/media/coronavirus-logo.4d888eda.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ffefd8baa02f692eeca31510d0d39c76",
+    "url": "/portfolio/static/media/heart.ffefd8ba.jpg"
+  },
+  {
+    "revision": "19e65b89cee273a249fba4c09b951b74",
+    "url": "/portfolio/static/media/ionicons.19e65b89.eot"
+  },
+  {
+    "revision": "2c159d0d05473040b53ec79df8797d32",
+    "url": "/portfolio/static/media/ionicons.2c159d0d.woff"
+  },
+  {
+    "revision": "dd4781d1acc57ba4c4808d1b44301201",
+    "url": "/portfolio/static/media/ionicons.dd4781d1.ttf"
+  },
+  {
+    "revision": "271efb738a5676e5fd501dc28b58320f",
+    "url": "/portfolio/static/media/photo-1.271efb73.jpg"
+  },
+  {
+    "revision": "e6e0d05cd5c4c50a6390cb3a766d901d",
+    "url": "/portfolio/static/media/photo-11.e6e0d05c.jpg"
+  },
+  {
+    "revision": "d57df505fa176242bc664aed3a75d5ff",
+    "url": "/portfolio/static/media/photo-13.d57df505.jpg"
+  },
+  {
+    "revision": "42cd556007de42fcbca1d553c3193fce",
+    "url": "/portfolio/static/media/photo-2.42cd5560.jpg"
+  },
+  {
+    "revision": "66fdd534fa29f94b339063f4cbd258d5",
+    "url": "/portfolio/static/media/photo-3.66fdd534.jpg"
+  },
+  {
+    "revision": "455dc5f644eef1cdf9b6fd87d73e8f5a",
+    "url": "/portfolio/static/media/photo-4.455dc5f6.jpg"
+  },
+  {
+    "revision": "52728d55d1c30241116835f415b33598",
+    "url": "/portfolio/static/media/photo-u1.52728d55.jpg"
+  },
+  {
+    "revision": "4a18305966983b50c0c61defda2be47b",
+    "url": "/portfolio/static/media/photo-u5.4a183059.jpg"
+  },
+  {
+    "revision": "4fc29212bd42883c45edd0bfbd91ad72",
+    "url": "/portfolio/static/media/poppins-v12-latin-regular.4fc29212.woff"
+  },
+  {
+    "revision": "9ed361bba8488aeb2797b82befda20f1",
+    "url": "/portfolio/static/media/poppins-v12-latin-regular.9ed361bb.woff2"
+  },
+  {
+    "revision": "00db023e23c2097358a2c52e095232a6",
+    "url": "/portfolio/static/media/w1.00db023e.jpg"
+  }
+]);
